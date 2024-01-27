60411d9fc4acd6e6402fcef472aedd76c531dd95
Mykola1612 <fermer2007pavlovich@gmail.com>
2024-01-27 11:19:41 +0100

    Summary

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcrypt";
 import User from "../db/userModel.js";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function register(req, res) {
   try {
@@ -18,7 +21,28 @@ export async function register(req, res) {
   }
 }
 
-export async function login(req, res) {}
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(409).send({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(409).send({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "23h" });
+
+    await User.findByIdAndUpdate(user._id, { token });
+
+    res.send({ token });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+    console.log(error);
+  }
+}
 
 export function getUser(req, res) {
   try {
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,7 +1,10 @@
 import User from "../db/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export async function authenticate(req, res, next) {
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export async function authentiBase(req, res, next) {
   try {
     const headers = req.headers.authorization.split(" ")[1];
     const [email, password] = Buffer.from(headers, "base64")
@@ -19,4 +22,24 @@ export async function authenticate(req, res, next) {
     res.status(401).send({ message: "Not authorized" });
     console.log(error);
   }
+}
+
+export async function authenticate(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, JWT_SECRET, async (error, data) => {
+    try {
+      if (error) {
+        throw new Error("Invalid token");
+      }
+      const user = await User.findById(data.id);
+      if (user.token !== token) {
+        throw new Error("Invalid user");
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      res.status(401).send({ message: error.message });
+      console.log(error);
+    }
+  });
 }
