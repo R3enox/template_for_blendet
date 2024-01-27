fb1db02e6f98d002ee7796a7282a24466454f79d
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-01-27 11:19:01 +0200

    add register and getUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,31 @@
-export async function register(req, res) {}
+import bcrypt from "bcrypt";
+import User from "../db/userModel.js";
+
+export async function register(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) return res.status(409).send({ message: "Email in use" });
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({ email, password: hashPassword });
+
+    res.status(201).send({ newUser });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+    console.log(error);
+  }
+}
 
 export async function login(req, res) {}
 
-export function getUser(req, res) {}
+export function getUser(req, res) {
+  try {
+    const { user } = req;
+    res.send({ user });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+    console.log(error);
+  }
+}
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,1 +1,22 @@
-export function authenticate(req, res, next) {}
+import User from "../db/userModel.js";
+import bcrypt from "bcrypt";
+
+export async function authenticate(req, res, next) {
+  try {
+    const headers = req.headers.authorization.split(" ")[1];
+    const [email, password] = Buffer.from(headers, "base64")
+      .toString()
+      .split(":");
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(401).send({ message: "Invalid username or password" });
+    const isMath = await bcrypt.compare(password, user.password);
+    if (!isMath)
+      return res.status(401).send({ message: "Invalid username or password" });
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ message: "Not authorized" });
+    console.log(error);
+  }
+}
